02c2054e85c2b13375d90e94b0d5f3be991c3d10
davidsean <david.sean@gmail.com>
2025-12-10 09:53:34 -0500

    add example test

---
+++ tests/testthat/test_andes_db_connect.R
@@ -0,0 +1,6 @@
+# test msg erreur
+testthat::test_that("Show error when input is bad", {
+
+  testthat::expect_error(get_access_table_properties(table_name = NULL), "Must supply a table name")
+
+})
